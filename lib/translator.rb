ccc0a61845c56abc7262a198b148f9de79d26a77
Nathan Bell <nathanbell09@gmail.com>
2018-11-13 14:22:44 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,11 @@ def get_japanese_emoticon(file_path, eng_emoticon)
   "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, jap_emoticon)
+  load_library(file_path).each do |get_keys, emote_hash|
+    emote_hash.each do |eng_emote, jap_emote|
+      jap_emote == jap_emoticon ? (return eng_emote) : nil
+    end
+  end
+  "Sorry, that emoticon was not found"
 end
