c77ef1dd56cafc30eab371e4ceaa3a33ad714cd7
Nathan Bell <nathanbell09@gmail.com>
2018-11-13 13:59:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,9 @@ file_path = "./lib/emoticons.yml"
 def load_library(file_path)
   emotes = YAML.load_file(file_path)
   loaded = emotes.each_with_object({}) do |(meaning, emote_array), loaded|
-    loaded["get_meaning"] = {}
-    loaded["get_emoticon"] = {}
     emote_array.each do |emote|
+      loaded["get_meaning"] ||= {}
+      loaded["get_emoticon"] ||= {}
       loaded["get_meaning"][emote[1]] = meaning
       loaded["get_emoticon"][emote[0]] = emote[1]
     end
